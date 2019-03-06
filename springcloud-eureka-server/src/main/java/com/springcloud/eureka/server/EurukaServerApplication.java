34a55caa37276087189d5c9d42d1571839c2c550
fanhou <qiuyedeshang@163.com>
2019-03-06 11:17:20 +0800

    first commit

---
+++ springcloud-eureka-server/src/main/java/com/springcloud/eureka/server/EurukaServerApplication.java
@@ -0,0 +1,17 @@
+package com.springcloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 开启Eureka的注册中心，只需要添加这个注解即可
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurukaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurukaServerApplication.class, args);
+    }
+}
